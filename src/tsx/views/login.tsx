33b578faeaa1c684fed0e3cdbe9511e509394544
Felix Mumme <felix.mumme@gmx.net>
2020-04-24 19:06:21 +0200

    initial login page

---
+++ src/tsx/views/login.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+
+export default function Login() {
+    return <>
+        <Typography variant="h3"> Login will appear soon</Typography>
+    </>
+}
