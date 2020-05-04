46ac13cace6fe5c816c9d236fa8d674f453f9759
Felix Mumme <felix.mumme@gmx.net>
2020-05-04 13:15:07 +0200

    added security config

--- src/tsx/tools/connections.js
+++ src/tsx/tools/connections.js
@@ -1,4 +1,4 @@
-const BACKEND_LOCAL = false
+export const BACKEND_LOCAL = true
 export const BACKEND_URL = BACKEND_LOCAL ? "http://localhost:8080" : "https://brainstorm-dhbw-backend.herokuapp.com"
 export const CORS_ANYWHERE = "https://cors-anywhere.herokuapp.com/"
 
--- src/tsx/tools/fetching.ts
+++ src/tsx/tools/fetching.ts
@@ -1,8 +1,16 @@
-import { BACKEND_URL, CORS_ANYWHERE } from "./connections";
+import { BACKEND_URL, CORS_ANYWHERE, BACKEND_LOCAL } from "./connections"
+
+const user = 'fe-tech-user'
+const pass = '+vq#3RL!ygE%f&HLM?t_'
+
+let headers = new Headers();
+headers.set('Authorization', 'Basic ' + new Buffer(user + ':' + pass).toString('base64'));
 
 export default function getFromBackend(path: string) {
-    return fetch(CORS_ANYWHERE + BACKEND_URL + path, {
-        method: 'GET'
+    const domain = (BACKEND_LOCAL ? '' : CORS_ANYWHERE) + BACKEND_URL;
+    return fetch(domain + path, {
+        method: 'GET',
+        headers: headers,
     })
 
 }
@@ -11,3 +19,6 @@ export function getJsonFromBackend(path: string) {
     return getFromBackend(path)
         .then(res => res.json());
 }
+
+
+
