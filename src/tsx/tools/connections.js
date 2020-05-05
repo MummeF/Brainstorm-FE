4c097a044d15d947fe86a3906e6fac27c542af71
Felix Mumme <felix.mumme@gmx.net>
2020-05-05 10:11:12 +0200

    descriptions

--- src/tsx/tools/connections.js
+++ src/tsx/tools/connections.js
@@ -3,8 +3,8 @@ export const BACKEND_URL = BACKEND_LOCAL ? "http://localhost:8080" : "https://br
 export const CORS_ANYWHERE = "https://fierce-beach-68272.herokuapp.com/"
 
 //Endpoints
-export const RND_ROOM_ID = "/rndRoomId"
-export const IS_ALIVE = "/isAlive"
-export const VAL_ROOM_ID = "/validateRoomId" //? roomId : long
-export const GET_ROOM = "/getRoom" //? roomId : long
-export const UPDT_ROOM = "/updateRoom" // body: Room
+export const RND_ROOM_ID = "/rndRoomId" //returns long
+export const IS_ALIVE = "/isAlive" //returns string => res.ok ? online : offline
+export const VAL_ROOM_ID = "/validateRoomId" //? roomId : long --- returns boolean
+export const GET_ROOM = "/getRoom" //? roomId : long --- returns RoomModel
+export const UPDT_ROOM = "/updateRoom" // body: Room --- returns string => res.ok ? success : denied
