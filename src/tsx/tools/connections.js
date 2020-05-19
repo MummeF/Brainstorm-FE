beebfff69c23d4785f4832f8ae421d173e17f5e2
Felix Mumme <felix.mumme@gmx.net>
2020-05-19 12:00:33 +0200

    finished pw

--- src/tsx/tools/connections.js
+++ src/tsx/tools/connections.js
@@ -1,4 +1,4 @@
-export const BACKEND_LOCAL = true
+export const BACKEND_LOCAL = false
 
 export const BACKEND_DOMAIN = BACKEND_LOCAL ? "localhost:8080" : "brainstorm-dhbw-backend.herokuapp.com"
 export const BACKEND_URL = (BACKEND_LOCAL ? "http" : "https") + "://" + BACKEND_DOMAIN
